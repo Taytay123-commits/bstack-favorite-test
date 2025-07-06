1aa16440f9ddd8207544818fbb0b938d6e377c26
Taytay123-commits <tayloraltman1@gmail.com>
2025-07-06 18:35:24 +0100

    Update test_favorite_device.py

--- tests/test_favorite_device.py
+++ tests/test_favorite_device.py
@@ -4,34 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-USERNAME = os.getenv("BROWSERSTACK_USERNAME")
-ACCESS_KEY = os.getenv("BROWSERSTACK_ACCESS_KEY")
-DEMO_USER = os.getenv("DEMO_USER")
-DEMO_PASS = os.getenv("DEMO_PASS")
-
-capabilities = [
-    {
-        "os": "Windows",
-        "osVersion": "10",
-        "browserName": "Chrome",
-        "browserVersion": "latest",
-        "name": "Windows Chrome Test"
-    },
-    {
-        "os": "OS X",
-        "osVersion": "Ventura",
-        "browserName": "Firefox",
-        "browserVersion": "latest",
-        "name": "macOS Firefox Test"
-    },
-    {
-        "deviceName": "Samsung Galaxy S22",
-        "realMobile": "true",
-        "platformName": "Android",
-        "name": "Samsung Galaxy S22 Test"
-    }
-]
+from browserstack_config import capabilities, USERNAME, ACCESS_KEY, DEMO_PASS
 
 @pytest.mark.parametrize("caps", capabilities)
 def test_favorite_galaxy_s20(caps):
@@ -41,36 +14,48 @@ def test_favorite_galaxy_s20(caps):
     caps["project"] = "Favorites Testing"
     caps["browserstack.debug"] = "true"
 
-    options = webdriver.ChromeOptions() if caps.get("browserName") == "Chrome" else webdriver.FirefoxOptions() if caps.get("browserName") == "Firefox" else None
+    browser_name = caps.get("browserName")
+
+    # Choose the appropriate options object for browser
+    options = None
+    if browser_name == "Chrome":
+        options = webdriver.ChromeOptions()
+    elif browser_name == "Firefox":
+        options = webdriver.FirefoxOptions()
+
     if options:
         for key, value in caps.items():
             options.set_capability(key, value)
+
+        driver = webdriver.Remote(
+            command_executor="http://hub.browserstack.com/wd/hub",
+            options=options
+        )
     else:
-        # Mobile testing - no options class used
-        options = caps
+        # For mobile caps, pass caps directly (no options)
+        driver = webdriver.Remote(
+            command_executor="http://hub.browserstack.com/wd/hub",
+            options=None
+        )
+        driver.capabilities.update(caps)
 
-    driver = webdriver.Remote(
-        command_executor="http://hub.browserstack.com/wd/hub",
-        options=options if isinstance(options, (webdriver.ChromeOptions, webdriver.FirefoxOptions)) else None,
-        desired_capabilities=None if isinstance(options, (webdriver.ChromeOptions, webdriver.FirefoxOptions)) else options
-    )
-
+    driver.get("https://bstackdemo.com")
     wait = WebDriverWait(driver, 10)
 
-    try:
-        driver.get("https://www.bstackdemo.com/")
-        wait.until(EC.visibility_of_element_located((By.ID, "username"))).send_keys(DEMO_USER)
-        driver.find_element(By.ID, "password").send_keys(DEMO_PASS)
-        driver.find_element(By.ID, "login-btn").click()
+    wait.until(EC.visibility_of_element_located((By.ID, "username"))).send_keys("demouser")
+    driver.find_element(By.ID, "password").send_keys(DEMO_PASS)
+    driver.find_element(By.ID, "login-btn").click()
 
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//div[text()='Samsung']"))).click()
-        wait.until(EC.presence_of_element_located((By.XPATH, "//p[text()='Samsung Galaxy S20+']")))
+    # Filter to Samsung
+    wait.until(EC.visibility_of_element_located((By.XPATH, "//span[text()='Samsung']"))).click()
 
-        heart_icon = driver.find_element(By.XPATH, "//p[text()='Samsung Galaxy S20+']/../..//span[contains(@class, 'favorite')]")
-        heart_icon.click()
+    # Favorite Galaxy S20+
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//p[text()='Samsung Galaxy S20+']/following-sibling::div//span[@class='wishlist']"))).click()
 
-        driver.find_element(By.ID, "favorites").click()
-        wait.until(EC.visibility_of_element_located((By.XPATH, "//p[text()='Samsung Galaxy S20+']")))
+    # Navigate to Favorites
+    driver.find_element(By.ID, "favorites").click()
 
-    finally:
-        driver.quit()
+    # Validate Galaxy S20+ appears
+    wait.until(EC.visibility_of_element_located((By.XPATH, "//p[text()='Samsung Galaxy S20+']")))
+
+    driver.quit()
