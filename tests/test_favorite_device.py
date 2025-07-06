500734faf3a838fedf9a56b070882b9bfba546e4
Taytay123-commits <tayloraltman1@gmail.com>
2025-07-06 20:31:31 +0100

    Update test_favorite_device.py

--- tests/test_favorite_device.py
+++ tests/test_favorite_device.py
@@ -5,33 +5,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from dotenv import load_dotenv
 
-load_dotenv()
-
-USERNAME = os.getenv("DEMO_USERNAME")
-PASSWORD = os.getenv("DEMO_PASSWORD")
-BROWSERSTACK_USERNAME = os.getenv("BROWSERSTACK_USERNAME")
-BROWSERSTACK_ACCESS_KEY = os.getenv("BROWSERSTACK_ACCESS_KEY")
+USERNAME = os.getenv("BROWSERSTACK_USERNAME")
+ACCESS_KEY = os.getenv("BROWSERSTACK_ACCESS_KEY")
+DEMO_USERNAME = os.getenv("DEMO_USERNAME")
+DEMO_PASSWORD = os.getenv("DEMO_PASSWORD")
 
 capabilities = [
     {
         "browserName": "Chrome",
         "browserVersion": "latest",
         "os": "Windows",
-        "osVersion": "10"
+        "osVersion": "10",
     },
     {
         "browserName": "Firefox",
         "browserVersion": "latest",
         "os": "OS X",
-        "osVersion": "Monterey"
+        "osVersion": "Ventura",
     },
     {
         "deviceName": "Samsung Galaxy S22",
         "realMobile": "true",
-        "osVersion": "12.0"
-    }
+        "osVersion": "12.0",
+    },
 ]
 
 @pytest.mark.parametrize("caps", capabilities)
@@ -39,8 +36,8 @@ def test_favorite_galaxy_s20(caps):
     browser_name = caps.get("browserName")
 
     bstack_options = {
-        "userName": BROWSERSTACK_USERNAME,
-        "accessKey": BROWSERSTACK_ACCESS_KEY,
+        "userName": USERNAME,
+        "accessKey": ACCESS_KEY,
         "buildName": "Galaxy Fav Build",
         "projectName": "Favorites Testing",
         "debug": "true"
@@ -48,52 +45,73 @@ def test_favorite_galaxy_s20(caps):
 
     caps["bstack:options"] = bstack_options
 
-    options = webdriver.ChromeOptions()
-    for key, value in caps.items():
-        options.set_capability(key, value)
+    try:
+        # Select appropriate driver
+        if browser_name in ["Chrome", "Firefox"]:
+            options = webdriver.ChromeOptions() if browser_name == "Chrome" else webdriver.FirefoxOptions()
+            for key, value in caps.items():
+                options.set_capability(key, value)
+            driver = webdriver.Remote(
+                command_executor="https://hub.browserstack.com/wd/hub",
+                options=options
+            )
+        else:
+            mobile_options = webdriver.ChromeOptions()
+            for key, value in caps.items():
+                mobile_options.set_capability(key, value)
+            driver = webdriver.Remote(
+                command_executor="https://hub.browserstack.com/wd/hub",
+                options=mobile_options
+            )
 
-    driver = webdriver.Remote(
-        command_executor="https://hub.browserstack.com/wd/hub",
-        options=options
-    )
+        wait = WebDriverWait(driver, 20)
+        driver.get("https://bstackdemo.com")
 
-    wait = WebDriverWait(driver, 20)
-    driver.get("https://bstackdemo.com")
+        # Click Sign In
+        wait.until(EC.element_to_be_clickable((By.ID, "signin"))).click()
 
-    # Login
-    wait.until(EC.element_to_be_clickable((By.ID, "signin"))).click()
+        # Enter username
+        username_container = wait.until(EC.presence_of_element_located((By.ID, "username")))
+        username_input = username_container.find_element(By.TAG_NAME, "input")
+        username_input.send_keys(DEMO_USERNAME)
+        username_input.send_keys(Keys.ENTER)
 
-    username_input = wait.until(EC.presence_of_element_located((By.ID, "username"))).find_element(By.TAG_NAME, "input")
-    username_input.send_keys(USERNAME)
-    username_input.send_keys(Keys.ENTER)
+        # Enter password
+        password_container = wait.until(EC.presence_of_element_located((By.ID, "password")))
+        password_input = password_container.find_element(By.TAG_NAME, "input")
+        password_input.send_keys(DEMO_PASSWORD)
+        password_input.send_keys(Keys.ENTER)
 
-    password_input = wait.until(EC.presence_of_element_located((By.ID, "password"))).find_element(By.TAG_NAME, "input")
-    password_input.send_keys(PASSWORD)
-    password_input.send_keys(Keys.ENTER)
+        # Click login button
+        login_button = wait.until(EC.element_to_be_clickable((By.ID, "login-btn")))
+        login_button.click()
 
-    wait.until(EC.element_to_be_clickable((By.ID, "login-btn"))).click()
-    wait.until(lambda d: "signin=true" in d.current_url)
-    assert "signin=true" in driver.current_url
+        # Confirm login by URL
+        wait.until(lambda d: "signin=true" in d.current_url)
+        assert "signin=true" in driver.current_url
 
-    # Click Samsung checkbox
-    samsung_checkbox_label = wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//label[input[@value='Samsung']]"))
-    )
-    samsung_checkbox_label.click()
+        # Click the Samsung checkbox
+        samsung_checkbox_label = wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//label[.//span[text()='Samsung']]"))
+        )
+        samsung_checkbox_label.click()
 
-    # Click Favorite on Galaxy S20+
-    favorite_btn = wait.until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "div[data-sku='samsung-S20+-device-info.png'] button[aria-label='delete']"))
-    )
-    favorite_btn.click()
+        # Click the heart icon to favorite the Galaxy S20+
+        fav_button = wait.until(EC.element_to_be_clickable((
+            By.XPATH,
+            "//p[text()='Galaxy S20+']/ancestor::div[contains(@class, 'shelf-item')]//button[@aria-label='delete']"
+        )))
+        fav_button.click()
 
-    # Navigate to Favourites
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a#favourites"))).click()
+        # Go to Favourites
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a#favourites"))).click()
 
-    # Check Galaxy S20+ is in favourites
-    fav_item = wait.until(
-        EC.presence_of_element_located((By.XPATH, "//p[contains(@class, 'shelf-item__title') and text()='Galaxy S20+']"))
-    )
-    assert fav_item.is_displayed()
+        # Confirm Galaxy S20+ is in favourites
+        fav_s20 = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.shelf-item[data-sku='samsung-S20+-device-info.png']"))
+        )
+        assert fav_s20.is_displayed()
 
-    driver.quit()
+    finally:
+        driver.quit()
+
